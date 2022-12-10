57afc710df5af12af29c92db885a1c7775b8df21
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-10 10:24:48 -0300

    main component maded

---
+++ components/Main/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
+
+const index = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-300">
+            Let's build something cool
+          </p>
+          <h1 className="py-4 text-gray-200">
+            Hey, I'm <span className="text-[#D72323]">Luciano</span>
+          </h1>
+          <h1>
+            A <span className="text-[#D72323]">Front-end</span> Developer
+          </h1>
+          <p className="py-4 text-gray-400 max-w-[70%] m-auto">
+            I'm a Front-end ReactJs Developer specializing in building digital
+            experiences. Currently im focused on building Fron-end web
+            applications while im learning Back-end technologies.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-700 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-700 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-700 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-700 p-6 cursor-pointer hover:scale-110 ease-in duration-200">
+              <BsPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
