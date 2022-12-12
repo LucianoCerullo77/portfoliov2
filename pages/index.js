3b2ea9ba789315a32ee764cb675d127e8d1b9e9a
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-12 11:16:08 -0300

    problem solved, contact component maded

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Main from "../components/Main";
 import AboutMe from "../components/AboutMe";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact"
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <AboutMe />
       <Skills />
       <Projects />
+      <Contact/>
     </div>
   );
 }
