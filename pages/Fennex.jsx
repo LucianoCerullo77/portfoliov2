69be250fb706e758d37e2750e669d49168253236
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-16 17:30:55 -0300

    screen size error solved

--- pages/Fennex.jsx
+++ pages/Fennex.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const index = () => {
   return (
     <div className="w-full">
-      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+      <div className="w-full h-[30vh] lg:h-[40vh] relative">
         <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-20" />
         <Image
           className="absolute z-10"
