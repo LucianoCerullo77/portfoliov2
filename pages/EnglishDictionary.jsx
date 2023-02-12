f89b6bf29e32659c37fdbac60dbd1feb1affa064
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-02-12 07:47:19 -0300

    testing solutions

--- pages/EnglishDictionary.jsx
+++ pages/EnglishDictionary.jsx
@@ -1,92 +1,6 @@
-// import Image from "next/image";
-// import React from "react";
-// import EnglishDictionary from "../public/englishdictionary.png";
-// import { RiRadioButtonFill } from "react-icons/ri";
-// import Link from "next/link";
-
-// const index = () => {
-//   return (
-//     <div className="w-full">
-//       <div className="w-full h-[30vh] lg:h-[40vh] relative">
-//         <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-20" />
-//         <Image
-//           className="absolute z-10"
-//           layout="fill"
-//           objectFit="cover"
-//           src={EnglishDictionary}
-//           priority="true"
-//           alt="/"
-//         />
-//         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-[#f5eded] z-20 p-2">
-//           <h2 className="py-2">English Dictionary</h2>
-//           <h3>HTML / CSS / JavaScript </h3>
-//         </div>
-//       </div>
-//       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
-//         <div className="col-span-4">
-//           <p>Project</p>
-//           <h2>English Dictionary</h2>
-//           <p className="py-2">
-//             This Project was maded because i was bored (like the dad joke
-//             generator), the site is quite simple, you only search the word that
-//             you want to know about and the site will show the meaning and a
-//             audio sample. The API that I've used was the Free Dictionary API
-//             (https://dictionaryapi.dev).
-//           </p>
-//           <button className="px-8 py-2 mt-4 mr-8">
-//             <a
-//               href="https://english-dictionary-two.vercel.app"
-//               target="_blank"
-//               rel="noreferrer"
-//             >
-//               Demo
-//             </a>
-//           </button>
-//           <button className="px-8 py-2 mt-4 mr-8">
-//             <a
-//               href="https://github.com/LucianoCerullo77/englishDictionary"
-//               target="_blank"
-//               rel="noreferrer"
-//             >
-//               Repository
-//             </a>
-//           </button>
-//         </div>
-//         <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-600 rounded-xl p-4">
-//           <div className="p-2">
-//             <p className="uppercase text-center font-bold pb-2">Technologies</p>
-//             <div className="grid grid-cols-3 md:grid-cols-1">
-//               <p className="text-gray-600 py-2 flex items-center">
-//                 <RiRadioButtonFill className="pr-1" /> HTML{" "}
-//               </p>
-//               <p className="text-gray-600 py-2 flex items-center">
-//                 <RiRadioButtonFill className="pr-1" /> CSS{" "}
-//               </p>
-//               <p className="text-gray-600 py-2 flex items-center">
-//                 <RiRadioButtonFill className="pr-1" /> JavaScript{" "}
-//               </p>
-//               <p className="text-gray-600 py-2 flex items-center">
-//                 <RiRadioButtonFill className="pr-1" /> Git{" "}
-//               </p>
-//               <p className="text-gray-600 py-2 flex items-center">
-//                 <RiRadioButtonFill className="pr-1" /> Github{" "}
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//         <Link href="/">
-//           <p className="underline cursor-pointer">Back</p>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default index;
-
 import Image from "next/image";
 import React from "react";
-import Olivia from "../public/Olivia.png";
+import EnglishDictionary from "../public/englishdictionary.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
@@ -99,34 +13,29 @@ const index = () => {
           className="absolute z-10"
           layout="fill"
           objectFit="cover"
-          src={Olivia}
+          src={EnglishDictionary}
           priority="true"
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-[#f5eded] z-20 p-2">
-          <h2 className="py-2">Sweet Olivia e-commerce</h2>
-          <h3>HTML / CSS / JavaScript / ReactJs / Bootstrap</h3>
+          <h2 className="py-2">English Dictionary</h2>
+          <h3>HTML / CSS / JavaScript </h3>
         </div>
       </div>
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
         <div className="col-span-4">
           <p>Project</p>
-          <h2>Sweet Olivia</h2>
+          <h2>English Dictionary</h2>
           <p className="py-2">
-            After finished my ReactJs Course and getting involved in the top 10%
-            of students, I've earned being able to participate in a team to make
-            a real project (again). This team was conformed by 2 UX/UI Designers
-            and 2 FrontEnd ReactJs Developers. Making this project was quite
-            fun, because I've being able to learn more from my team-mates.
-            Making more Daily's and started with Pair-Programming. Also making
-            new friends and colleagues. For this project we used ReactJs as main
-            FrameWork and React-Bootstrap to give stylization to the site. We
-            make and simple e-commerce using Styled-Components and we consume an
-            JSON with the data. One of my best projects.
+            This Project was maded because i was bored (like the dad joke
+            generator), the site is quite simple, you only search the word that
+            you want to know about and the site will show the meaning and a
+            audio sample. The API that I've used was the Free Dictionary API
+            (https://dictionaryapi.dev).
           </p>
           <button className="px-8 py-2 mt-4 mr-8">
             <a
-              href="https://sweet-olivia-eight.vercel.app"
+              href="https://english-dictionary-two.vercel.app"
               target="_blank"
               rel="noreferrer"
             >
@@ -135,7 +44,7 @@ const index = () => {
           </button>
           <button className="px-8 py-2 mt-4 mr-8">
             <a
-              href="https://github.com/JoaquinBruhn/sweet-olivia"
+              href="https://github.com/LucianoCerullo77/englishDictionary"
               target="_blank"
               rel="noreferrer"
             >
@@ -157,18 +66,6 @@ const index = () => {
                 <RiRadioButtonFill className="pr-1" /> JavaScript{" "}
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> ReactJs{" "}
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> React-Bootstrap{" "}
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Bootstrap{" "}
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> WhatsApp-React-Component{" "}
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill className="pr-1" /> Git{" "}
               </p>
               <p className="text-gray-600 py-2 flex items-center">
@@ -186,4 +83,3 @@ const index = () => {
 };
 
 export default index;
-
